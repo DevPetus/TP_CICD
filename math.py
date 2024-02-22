ed1e7eb3a7af1bab37d95ea013fb362ccccad246
ebourgeo <devebourgeo@gmail.com>
2024-02-22 17:40:42 +0100

    :art: Linting for PyLint

--- math.py
+++ math.py
@@ -1,19 +1,27 @@
+"""Module for testing the Github Actions workflow"""
+
 import unittest
 
 class SimpleMath:
-    def addition(a, b):
+    """class for storing simple math functions"""
+    def addition(self, a, b):
+        """Addition function, return the sum of its two arguments a and b."""
         return a+b
 
-    def subtraction(a, b):
+    def subtraction(self, a, b):
+        """Subtraction function, return the difference of its two arguments a and b."""
         return a-b
 
 class TestSimpleMath(unittest.TestCase):
+    """UT class for SimpleMath"""
     def testadd(self):
-        res = SimpleMath.addition(3, 2)
+        """testing function for SimpleMath.addition"""
+        res = SimpleMath.addition(self, 3, 2)
         self.assertEqual(res, 5)
 
     def testsub(self):
-        res = SimpleMath.subtraction(3, 2)
+        """testing function for SimpleMath.subtraction"""
+        res = SimpleMath.subtraction(self, 3, 2)
         self.assertEqual(res, 1)
 
 if __name__ == "__main__":
