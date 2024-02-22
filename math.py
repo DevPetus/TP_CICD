3357b1ab9aaac3227c98291c4825118ef7a1b73e
ebourgeo <devebourgeo@gmail.com>
2024-02-22 17:23:59 +0100

    :bug: Fixing test

--- math.py
+++ math.py
@@ -5,6 +5,9 @@ class SimpleMath:
         return a+b
 
 class TestSimpleMath(unittest.TestCase):
-    def testadd():
-        res = addition(3, 2)
+    def testadd(self):
+        res = SimpleMath.addition(3, 2)
         self.assertEqual(res, 5)
+
+if __name__ == "__main__":
+    unittest.main()
