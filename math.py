53d4a6d5f6b9d58f11df1254eee7d09538048a35
ebourgeo <devebourgeo@gmail.com>
2024-02-22 17:27:19 +0100

    :sparkles: adding subtraction

--- math.py
+++ math.py
@@ -4,10 +4,17 @@ class SimpleMath:
     def addition(a, b):
         return a+b
 
+    def subtraction(a, b):
+        return a-b
+
 class TestSimpleMath(unittest.TestCase):
     def testadd(self):
         res = SimpleMath.addition(3, 2)
         self.assertEqual(res, 5)
+
+    def testsub(self):
+        res = SimpleMath.subtraction(3, 2)
+        self.assertEqual(res, 1)
 
 if __name__ == "__main__":
     unittest.main()
